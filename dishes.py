0d28ffadb074ec3b39bca762c0bbbce8ba2877d3
Sv net <Sveta19930323@gmail.com>
2025-01-07 14:27:39 +0300

    Full work Open files

--- dishes.py
+++ dishes.py
@@ -1,8 +1,6 @@
 ﻿
 import pprint
 
-
-
 # в файле dishes.txt хранится список блюд: название, ингредиенты и их количество
 # Заданеи № 1 :читаем список блюд
 
@@ -20,7 +18,8 @@ def cook_dishes (f):
 
 with open('dishes.txt', encoding='utf-8-sig') as f:
     cook_book = cook_dishes(f)
-    print('cook_book = ', cook_book, sep='\n')
+    print ('cook_book = ')
+    pprint.pprint(cook_book)
 
 
 # Задание № 2: готовим блюда на определенное количество гостей.
@@ -40,5 +39,6 @@ def get_shop_list_by_dishes(dishes, persons):
 
     return get_dishes
 
+print()
 print(get_shop_list_by_dishes(['Омлет', 'Утка по пекински'], 2))
 
